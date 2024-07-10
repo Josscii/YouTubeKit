529485d6688ad52968bb897b61d59519d5e6a0a5
josscii <jossciiweiyi@gmail.com>
2024-07-10 12:49:21 +0800

    expose videoResolution

--- Sources/YouTubeKit/Models/ITag.swift
+++ Sources/YouTubeKit/Models/ITag.swift
@@ -12,7 +12,7 @@ public struct ITag {
     let itag: Int
     
     /// lower side of resolution (e.g. 1080 for 1080p)
-    let videoResolution: Int?
+    public let videoResolution: Int?
     
     /// in kbps
     let audioBitrate: Int?
