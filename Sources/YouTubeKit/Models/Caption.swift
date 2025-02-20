9ed7d9dd353be17107b49a21461a57fff92175bf
josscii <jossciiweiyi@gmail.com>
2025-02-20 17:59:32 +0800

    add captions

---
+++ Sources/YouTubeKit/Models/Caption.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  YouTubeKit
+//
+//  Created by Josscii on 2025/2/20.
+//
+
+import Foundation
+
+@available(iOS 13.0, watchOS 6.0, tvOS 13.0, macOS 10.15, *)
+public struct Caption: Sendable {
+    public let url: String
+    public let name: String
+}
